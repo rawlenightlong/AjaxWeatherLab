980d3e36ea581a83a3af44db2c519b9cee2b4276
Rawle Becerril <rawlesxm@gmail.com>
2022-10-24 22:42:38 -0400

    geocoder functionality

--- app.js
+++ app.js
@@ -6,29 +6,38 @@ const apiKey = "ee1f44a4b1bd4badd8adbddf5f0d08d0"
 
 console.log(baseUrl, apiKey)
 
-function weatherSearch(city){
-    const url = `${baseUrl}?q=${city}&APPID=${apiKey}`
+function weatherSearch(city, state){
+    const url = `http://api.openweathermap.org/geo/1.0/direct?q=${city},${state},us&limit=5&appid=ee1f44a4b1bd4badd8adbddf5f0d08d0`
     console.log(url)
 
     $.ajax(url)
-    .then((weather) => {
-        console.log(weather)
+    .then((coordinates) => {
+        console.log(coordinates[0].lat)
+        console.log(coordinates[0].lon)
 
-        const $divCity = $("div.city")
-        $divCity.empty()
-        const $divTemperature = $("div.temperature")
-        $divTemperature.empty()
-        const $divFeelsLike = $("div.feelsLike")
-        $divFeelsLike.empty()
-        const $divWeather = $("div.weather")
-        $divWeather.empty()
-        // console.log(weather.name)
-        // console.log(weather.weather[0].description)
+        const lat = coordinates[0].lat
+        const lon = coordinates[0].lon
+        const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=ee1f44a4b1bd4badd8adbddf5f0d08d0`
 
-        $divCity.append(`Weather in: ${weather.name}`)
-        $divTemperature.append("Temperature: " + Math.floor((((weather.main.temp-273.15)*1.8)+32)) + " degrees Fahrenheit")
-        $divFeelsLike.append("Feels Like: " + Math.floor((((weather.main.feels_like-273.15)*1.8)+32)) + " degrees Fahrenheit") 
-        $divWeather.append("Weather Description: " + weather.weather[0].description)
+        $.ajax(weatherUrl)
+        .then((weather) => {
+            
+            const $divCity = $("div.city")
+            $divCity.empty()
+            const $divTemperature = $("div.temperature")
+            $divTemperature.empty()
+            const $divFeelsLike = $("div.feelsLike")
+            $divFeelsLike.empty()
+            const $divWeather = $("div.weather")
+            $divWeather.empty()
+
+            console.log(weather)
+
+            $divCity.append(`Weather in: ${weather.name}, ${state.toUpperCase()}`)
+            $divTemperature.append("Temperature: " + Math.floor((((weather.main.temp-273.15)*1.8)+32)) + " degrees Fahrenheit")
+            $divFeelsLike.append("Feels Like: " + Math.floor((((weather.main.feels_like-273.15)*1.8)+32)) + " degrees Fahrenheit") 
+            $divWeather.append("Weather Description: " + weather.weather[0].description)
+        })
     })
 }
 
@@ -38,13 +47,14 @@ $("input[type=submit]").on("click", (event) => {
     // prevent refresh
     event.preventDefault()
     // grab text from input
-    const inputText = $("input[type=text]").val()
+    const cityText = $("input[type=text].city").val()
+    const stateText = $("input[type=text].state").val()
     //update screen
-    weatherSearch(inputText)
+    weatherSearch(cityText, stateText)
     
 } )
 
-// weatherSearch("New York")
+weatherSearch("bowie", "md")
 
 
 
